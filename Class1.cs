d2eea7fb7170a9a34676f07bd3087e80975425cd
NaveenPulamolu <naveenp992@gmail.com>
2016-12-24 16:43:44 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class TestClass<T>
+    {
+        private T a; // Declared private so is not accesible by other classes
+        public TestClass(T t)
+        {
+            a = t;
+        }
+        public void Write()
+        {
+            Console.WriteLine(this.a);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass<int> test1 = new TestClass<int>(25);//adding int data to generic
+            test1.Write();
+            TestClass<string> test2 = new TestClass<string>("Naveen");// adding string data to generic
+            test2.Write();
+            Console.ReadLine();
+        }
+    }
+}
